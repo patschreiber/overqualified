fd84a5412e291fd43d81d5268cbbc369e835b809
patricks <patrick@nori.nu>
2014-05-08 23:05:02 -0500

    Added screen width specific javascript file

---
+++ app/assets/javascripts/screen-width.js
@@ -0,0 +1,12 @@
+/* =======================
+    This file is used to execute javascript on different screen sizes.
+    
+========================== */ 
+
+$(document).ready(function() {
+
+  if (screen.width < 768) {
+    $('.navbar-toggle').fadeIn(); 
+  }
+
+});
