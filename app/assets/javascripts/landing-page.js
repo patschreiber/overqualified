ee78f091287ae7bb9789374bba83af1c1147baaa
patricks <patrick@nori.nu>
2014-05-09 04:35:44 -0500

    syle work

---
+++ app/assets/javascripts/landing-page.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  var jumboHeight = $('.jumbotron').outerHeight();
+  
+  function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+  }
+
+  $(window).on('scroll', function(e){
+      parallax();
+  });
+
+});
