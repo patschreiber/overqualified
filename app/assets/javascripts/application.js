b74a548d31c9b1c13addb93933d4d41bcb20b422
Patrick Schreiber <ptos382@gmail.com>
2014-06-13 01:17:35 -0500

    Redid main nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,4 @@
 //= require_tree .
 
 //= require modernizr
+//= require hoverIntent
--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-
+  $('nav').hoverIntent({
+    over: expand,
+    out: expand,
+    interval: 50
+  });
 });
+
+function expand() {
+  $('.nav-menu').toggleClass("nav-menu-open");
+  $('body').toggleClass("nav-menu-open-body");
+}
+
+//    $('.nav-menu').toggleClass("nav-menu-open");
+   // $('body').toggleClass("nav-menu-open-body");
