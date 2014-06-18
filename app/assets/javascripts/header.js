fd8a22be2cdc818a50248cf791a7b78896228e23
Patrick Schreiber <ptos382@gmail.com>
2014-06-17 22:30:29 -0500

    Mobile nav is in the bag

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
   
   $('nav').hover(expand);
+
+  $('.navbar-toggle').on('click', function() {
+    $('#mobile-navbar-menu').stop().slideToggle();
+  });
 });
 
 function expand() {
   $('body').toggleClass("nav-menu-collapsed");
-  $('.collapsed-nav-logo').toggleClass("collapsed-nav-logo-hover");
+  $('.collapsed-side-nav').toggleClass("collapsed-side-nav-hover");
 }
