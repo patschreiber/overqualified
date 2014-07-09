3c8e867840b545ba99c26d84438a5e563aa24f68
Patrick Schreiber <ptos382@gmail.com>
2014-07-09 04:11:00 -0500

    Added development-only seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Seeding the database..."
+
+# Reset the seeds for static data
+
+
+# Runs certain seeds against development or production
+case Rails.env
+when "production"
+when "development"
+  Item.delete_all
+
+  # Seed the development database with test items
+  lambda {
+    item = Item.new
+    item.id = 10001
+    item.name = "Test Men's Shirt"
+    item.gender = "Male"
+    item.description = "Objective: To obtain a position in the industry"
+    item.save!
+  }.call 
+
+  lambda {
+    item = Item.new
+    item.id = 10002
+    item.name = "Test Women's Shirt"
+    item.gender = "Female"
+    item.description = "No Real World Experience"
+    item.save!
+  }.call 
+
+  lambda {
+    item = Item.new
+    item.id = 10003
+    item.name = "Test Unisex Shirt"
+    item.gender = "Unisex"
+    item.description = "Great Oral and Written Communication Skills"
+    item.save!
+  }.call 
+end
